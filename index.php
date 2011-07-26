88e455d687a66ef9208952ebc942cdc55d17fbfe
Jason Taikato <jasontaikato@gmail.com>
2011-07-26 22:18:01 +1000

    Change to FACT questionnaire

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<h1>Log In</h1>
 		</header>
 		<section id="login">
-			<form action="question1.php">
+			<form action="question1a.php">
 				<label for="idNumber">ID Number:</label>
 				<input type="password" id="idNumber" name="idNumber" placeholder="your barcode number" /><br >
 				<input class="login" type="submit" value="Log in" />
---
+++ question1a.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<?php include("includes/head.php"); ?>
+	
+	<body lang="en">
+		<section id="login">
+			<form action="question1b.php" method="get">
+				<fieldset>
+					<h2>Physical Well-Being</h2>
+					<p class="question">I have a lack of energy</p>
+					<label class="labelRadio" for="radio-1"><input type="radio" id="radio-1" name="radioHealth" value="Not at all" />Not at all</label>
+					<label class="labelRadio" for="radio-2"><input type="radio" id="radio-2" name="radioHealth" value="A little bit" />A little bit</label>
+					<label class="labelRadio" for="radio-3"><input type="radio" id="radio-3" name="radioHealth" value="Somewhat" />Somewhat</label>
+					<label class="labelRadio" for="radio-4"><input type="radio" id="radio-4" name="radioHealth" value="Quite a bit" />Quite a bit</label>
+					<label class="labelRadio" for="radio-5"><input type="radio" id="radio-5" name="radioHealth" value="Very much" />Very much</label>
+				</fieldset>
+					<input type="submit" value="Next" class="next" disabled="disabled" />
+			</form>
+		</section>
+		<?php include("includes/footer.php"); ?>
+	</body>
+	
+</html>
